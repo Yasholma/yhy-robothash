3d496ffe7b5b7c792410b747318f51ab2a1d5607
Cybertech <yasholma1@gmail.com>
2020-03-13 20:06:05 +0100

    Updates

---
+++ precache-manifest.c108edf39bf540bfc190c59c0fdaaf20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe0858b104b96945d7c679c54830ac8",
+    "url": "/yhy-robothash/index.html"
+  },
+  {
+    "revision": "2aaea843863fbe07c3db",
+    "url": "/yhy-robothash/static/css/main.f8a6771c.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/yhy-robothash/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/yhy-robothash/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaea843863fbe07c3db",
+    "url": "/yhy-robothash/static/js/main.4dbef61f.chunk.js"
+  },
+  {
+    "revision": "f73ba9bbcfb32961176c",
+    "url": "/yhy-robothash/static/js/runtime-main.a661e08a.js"
+  }
+]);
